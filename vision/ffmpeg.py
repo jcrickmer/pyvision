471eed44fbbee1827e9603c99b9e87dba19114d1
Jason Crickmer <jcrickmer@gmail.com>
2018-09-05 09:08:38 -0500

    Quality can now be passed in during frame extraction in order to change the quality of the JPEG frames, or the compression/data size of the frames.

--- vision/ffmpeg.py
+++ vision/ffmpeg.py
@@ -24,7 +24,25 @@ def which(program):
     return None
 
 class extract(object):
-    def __init__(self, path, fps = None, size = None):
+    def __init__(self, path, fps = None, size = None, quality = None):
+        """
+        Extract frames from a video using ffmpeg (or avconv). Provide access to
+        those frames via a collection of PIL.Image objects.
+
+        Args:
+            path (str): the file path to the video to extract frames from.
+            fps (int): ask ffmpeg to extract images at a specific frame rate.
+                If None, then let ffmpeg follow its default behavior, which is
+                to use the FPS indicated in the video.
+            size (2-item tuple of ints): ask ffmpeg to resize the image to a
+                specific frame size, in pixels. Note that this does not
+                preserve aspect ratio.
+            quality (int): a value between 2 and 31, the represents the
+                level of lossy compression to use on the resulting JPEG frame.
+                2 is the least compression, highest quality. 31 is the most
+                compression and loss. If None, then let ffmpeg do its default
+                thing (which appears to be 2, but may be ffmpeg-dependent).
+        """
         self.key = int(random.random() * 1000000000)
         self.key = "pyvision-ffmpeg-{0}".format(self.key)
         self.output = "/tmp/{0}".format(self.key)
@@ -43,7 +61,12 @@ class extract(object):
         if size:
             w, h = size
             cmd = "{0} -s {1}x{2}".format(cmd, int(w), int(h))
-        os.system("{0} {1}/%d.jpg".format(cmd, self.output))
+        if quality:
+            # quality should be an int between 2 and 31. 2 is BEST, 31 is WORST
+            cmd = "{0} -q {1}".format(cmd, int(quality))
+
+        cmd = "{0} {1}/%d.jpg".format(cmd, self.output)
+        os.system(cmd)
 
     def __del__(self):
         if self.output:
